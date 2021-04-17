79f20e4d3e12f8e68db0e79a5af51cbae8015c2e
Camila <camvives@gmail.com>
2021-04-17 16:31:16 -0300

    Ejercicio 13 resuelto

--- practico_01/ejercicio_13.py
+++ practico_01/ejercicio_13.py
@@ -11,6 +11,7 @@ otra manera.
 
 from typing import Iterator, Callable
 
+initial = 0
 
 def generar_pares_clousure(initial: int = 0) -> Callable[[], int]:
     """Toma un número inicial y devuelve una función que cada vez que es
@@ -21,8 +22,21 @@ def generar_pares_clousure(initial: int = 0) -> Callable[[], int]:
         - Usar closures
         - Usar el modificador nonlocal
     """
-    pass # Completar
 
+    if initial % 2  == 0:
+        x = initial - 2
+    else:
+        x = initial - 1
+    
+    def funcion2():
+        nonlocal x
+        if x % 2  == 0:
+            x += 2
+        else:
+            x += 1
+        return x
+
+    return funcion2
 
 # NO MODIFICAR - INICIO
 generador_pares = generar_pares_clousure(0)
@@ -32,7 +46,7 @@ assert generador_pares() == 4
 # NO MODIFICAR - FIN
 
 
-###############################################################################
+# ###############################################################################
 
 
 """Este tipo de comportamiento es conocido com semi-corutina, las semi-corutinas
@@ -45,7 +59,12 @@ def generar_pares_generator(initial: int = 0) -> Iterator[int]:
     """Re-Escribir utilizando Generadores
     Referencia: https://docs.python.org/3/howto/functional.html?highlight=generator#generators
     """
-    pass # Completar
+    while True:
+        yield initial
+        if initial % 2  == 0:
+            initial += 2
+        else:
+            initial += 1
 
 
 # NO MODIFICAR - INICIO
@@ -61,8 +80,16 @@ assert next(generador_pares) == 4
 
 def generar_pares_generator_send(initial: int = 0) -> Iterator[int]:
     """CHALLENGE OPCIONAL: Re-Escribir utilizando send para saltear numeros"""
-    pass # Completar
+    while True:
+        x = yield initial
 
+        if initial % 2 == 0:
+            initial += 2
+        else:
+            initial += 1
+
+        if type(x) == int:
+            initial = x
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
@@ -82,7 +109,8 @@ if __name__ == "__main__":
 
 def generar_pares_delegados(initial: int = 0) -> Iterator[int]:
     """CHALLENGE OPCIONAL: Re-Escribir utilizando Generadores delegados (yield from)"""
-    pass # Completar
+    
+    yield from generar_pares_generator()
 
 
 # NO MODIFICAR - INICIO
